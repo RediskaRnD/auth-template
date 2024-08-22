f6b4acd588d8916a77b5f524a3e6d6cb3e779799
Rediska <andrej.balakin@gmail.com>
2024-08-22 16:05:26 +0200

    add SignOut button to settings.tsx after SignOut, SignIn page don't show "Invalid credentials" error.

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,22 @@
 import { ReactElement } from 'react';
 
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
+import { Button } from '@/components/ui/button';
 
 const SettingsPage = async (): Promise<ReactElement> => {
   const session = await auth();
   return (
     <div>
       {JSON.stringify(session)}
+      <form action={async () => {
+        'use server';
+
+        await signOut();
+      }}>
+        <Button type="submit">
+          Sign Out
+        </Button>
+      </form>
     </div>
   );
 };
